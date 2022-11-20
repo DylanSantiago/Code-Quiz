d5f76e6c34f5a4499a557dbee5fd36c806081c3f
DylanSantiago <2dylansantiago@gmail.com>
2022-11-20 02:36:52 -0500

    html template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,28 @@
 var questions = [
     {
         question: "How would you write 'Hello World' in an alert box?",
-        answers: {
-            a: 'alertUser("Hello World")',
-            b: 'alertPrompt("Hello World")',
-            c: 'promptUser("Hello World")',
-            d: 'alert("Hello World")',
-        },
-        correctAnswer: 'd'
+        answers: ['alertUser("Hello World")','alertPrompt("Hello World")','promptUser("Hello World")', 'alert("Hello World")'],
+        correctAnswer: 3      
+      
     },
     {
         question: "The proper comment syntax in Javascript is",
-        answers: {
-            a: '// This is a comment',
-            b: '"This is a comment"',
-            c: '<!-- This is a comment -->',
-            d: '${This is a comment}',
-        },
-        correctAnswer: 'a'
+        answers: ['// This is a comment','"This is a comment"', '<!-- This is a comment -->','${This is a comment}'],
+        correctAnswer: 0
     },
     {
         question: "Which SQL statement is used to extract data from a database?",
-        answers: {
-            a: 'OBTAIN',
-            b: 'EXTRACT',
-            c: 'SELECT',
-            d: 'RETRIEVE',
-        },
-        correctAnswer: 'c'
+        answers: ['OBTAIN', 'EXTRACT', 'SELECT', 'RETRIEVE'],
+        correctAnswer: 2
     },
     {
         question: "How do you call a function named 'myFunction'?",
-        answers: {
-            a: 'function()',
-            b: 'myfunction()',
-            c: 'call myFunction()',
-            d: 'myFunction()',
-        },
-        correctAnswer: 'd'
+        answers: ['function()', 'myfunction()','call myFunction()','myFunction()'],    
+        correctAnswer: 3
     },
     {
         question: "The correct way to write a JavaScript array is?",
-        answers: {
-            a: 'var cars =["BMW", "Audi", "Mercedes"]',
-            b: 'var cars = {"BMW", "Audi", "Mercedes"}',
-            c: 'var cars = ("BMW", "Audi", "Mercedes")',
-            d: 'var cars = "BMW", "Audi", "Mercedes"',
-        },
-        correctAnswer: 'a'
+        answers: ['var cars =["BMW", "Audi", "Mercedes"]', 'var cars = {"BMW", "Audi", "Mercedes"}', 'var cars = ("BMW", "Audi", "Mercedes")', 'var cars = "BMW", "Audi", "Mercedes"'],
+        correctAnswer: 0
     },
 ]
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-    
-    function showQuestions(questions, quizContainer) {
-        var output = [];
-        var answers;
-
-        for(var i = 0; i<questions.length; i++){
-            answers = [];
-
-            for(letter in question[i].answers) {
-
-                answers.push(
-                    '<label>'
-                        +'<input type="radio" name="question'+i+'"value"'+letter+'">'
-                        + letter + ':'
-                        + questions[i].answers[letter]
-                    +'</label>'
-                );
-            }
-        }
-
-        output.push(
-            'div class="question">' + questions[i].question + '</div>'
-            +'<div class="answers">' + answers.join('') + '</div>'
-        );
-
-        quizContainer.innerHTML = output.join('');
-    }
-
-
-    function showResults(questions, quizContainer, resultsContainer) {
-
-    }
-
-    showQuestions(questions, quizContainer);
-
-
-    submitButton.onclick = function(){
-        showResults(questions, quizContainer, resultsContainer);
-    }
-}
